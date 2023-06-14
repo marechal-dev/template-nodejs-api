7538658c3836ace27c24deb2b5ed240fbacd6245
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-06-14 15:59:39 -0300

    feat: change "replace" to "optional"

--- src/configs/env/index.ts
+++ src/configs/env/index.ts
@@ -1,19 +1,19 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import 'dotenv/config';
+import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'] as const)
     .default('development'),
   PORT: z.coerce.number().default(3333),
-})
+});
 
-const parsingResult = envSchema.safeParse(process.env)
+const parsingResult = envSchema.safeParse(process.env);
 
 if (!parsingResult.success) {
-  console.error('❌ Invalid Env. Variables', parsingResult.error.format())
+  console.error('❌ Invalid Env. Variables', parsingResult.error.format());
 
   process.exit(1);
 }
 
-export const env = parsingResult.data
+export const env = parsingResult.data;
